1dc7d2e5bb9fb1a847deff45c81718e0960d96d0
nologyRob <rob.hooper@nology.io>
2023-02-17 17:00:32 +0000

    changed firebase file to not include file

--- src/firebase.js
+++ src/firebase.js
@@ -5,7 +5,7 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyDaXnJm1KRBXqWbXStcX538G7hOKkexsNY",
+    apiKey: process.env.REACT_APP_FIREBASE_API,
     authDomain: "sable-client-project.firebaseapp.com",
     projectId: "sable-client-project",
     storageBucket: "sable-client-project.appspot.com",
