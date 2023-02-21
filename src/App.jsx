b8cc7406d2300c83ed743142eae2b9fde7a16cb1
Becky Rushton <114007276+BeckyRushton@users.noreply.github.com>
2023-02-21 15:40:51 +0000

    Feature 53 color variables (#14)
    * added color variables to stylesheet
    
    * removed main style sheet as importing partials wasn't working
    
    * added more colour variables
    
    * removed use main in nav item
    
    * removed use main from App.jsx
    
    ---------
    
    Co-authored-by: navina321 <81630548+navina321@users.noreply.github.com>

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import Login from "./pages/Login/Login.jsx";
-import "./styles/main.scss";
+
 const App = () => {
   return (
     <Routes>
