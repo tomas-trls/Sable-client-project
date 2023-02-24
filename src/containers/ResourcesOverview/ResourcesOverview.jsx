60b1b72ca6801833b6b1bf8baaf067c32b46afbe
DexterFreeman <67049776+DexterFreeman@users.noreply.github.com>
2023-02-24 11:18:18 +0000

    quick save

--- src/containers/ResourcesOverview/ResourcesOverview.jsx
+++ src/containers/ResourcesOverview/ResourcesOverview.jsx
@@ -1,4 +1,10 @@
 import React from 'react'
+import FilterIcon from "../../assets/images/functional-icons/filter-icon.png";
+import gridview from "../../assets/images/functional-icons/gridview-icon.png";
+import listview from "../../assets/images/functional-icons/listview-icon.png";
+import SortIcon from "../../assets/images/functional-icons/sort-icon.png";
+import StaffImage from "../../assets/images/users/staff-picture.png";
+import DataCard from "../../components/DataCard/DataCard";
 import "./ResourcesOverview.scss"
 const ResourcesOverview = () => {
   return (
@@ -18,17 +24,29 @@ const ResourcesOverview = () => {
             alt="Grid view icon"
           />
         </div>
-        <input className="staff-overview__searchbar" type="text" name="" id="" />
+        <input
+          className="staff-overview__searchbar"
+          type="text"
+          name=""
+          id=""
+        />
         <div className="staff-overview__sort">
-          <img className="staff-overview__icon" src={SortIcon} alt="sort icon" />
+          <img
+            className="staff-overview__icon"
+            src={SortIcon}
+            alt="sort icon"
+          />
           <p className="staff-overview__subheading">Sort</p>
         </div>
         <div className="staff-overview__filter">
-          <img className="staff-overview__icon" src={FilterIcon} alt="filter icon" />
+          <img
+            className="staff-overview__icon"
+            src={FilterIcon}
+            alt="filter icon"
+          />
           <p className="staff-overview__subheading">Filter</p>
- 
-      </div>
 
+     
         </div>
     </div>
   )
