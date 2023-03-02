95db480ab91c675d8a274456ac558655a10d84e5
Dexter Freeman <67049776+DexterFreeman@users.noreply.github.com>
2023-03-02 11:19:47 +0000

    Feature 26 book appointment submit button functionality (#46)
    * removing h1
    
    * feature: added logout functionality to settings page logout button
    
    * started error page
    
    * created error page, starting on tests
    
    * test: added tests for error page
    
    * removed white space, and removed line in the test
    
    * forgot which branch I was on and changed the logout function whoops
    
    * Added submit functionality
    
    * Changed submittion so it doesn't take everything through state
    
    * removed cancel button :(
    
    * changed options to optionsJSX
    
    * adding input validation
    
    * feature: added input validation to the input fields, on an invalid input an alert with the appropriate error message is shown
    
    * changed InputField
    
    * Added requested changes and pulled from main

--- src/components/DropdownField/DropdownField.jsx
+++ src/components/DropdownField/DropdownField.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
 import "./DropdownField.scss"
 
-const DropdownField = (props) => {
-  const {label, placeholderText, id, handleInput, optionsJSX} = props
+const DropdownField = ({label, placeholderText, id, handleInput, optionsJSX}) => {
+  const JSX = optionsJSX.map((option, index) => {
+    return <option key={index+1}>{option}</option>
+  })
+
 return (
   <div className='dropdown-field'>
       <label className='dropdown-field__label' htmlFor={id}>{label}</label>
-      <select className="dropdown-field__input" id={id} placeholder={placeholderText}  onChange={handleInput}>{optionsJSX}</select> 
+      <select className="dropdown-field__input" id={id} placeholder={placeholderText} onChange={handleInput}>
+        {JSX}
+      </select>
   </div>
 )
 }
--- src/containers/BookingOverview/BookingOverview.jsx
+++ src/containers/BookingOverview/BookingOverview.jsx
@@ -5,41 +5,100 @@ import DatePicker from "react-date-picker";
 import Button from "../../components/Button/Button";
 import DropdownField from "../../components/DropdownField/DropdownField";
 import InputField from "../../components/InputField/InputField";
-
+import mockData from "../../data/mockData";
 import "./BookingOverview.scss";
-
 const UserInfoContainer = () => {
   const [day, setDay] = useState(new Date());
+  const [time, setTime] = useState("09:00");
+  const time_slots = [
+    "09:00",
+    "09:30",
+    "10:00",
+    "10:30",
+    "11:00",
+    "11:30",
+    "12:00",
+    "12:30",
+    "13:00",
+    "13:30",
+    "14:00",
+    "14:30",
+    "15:00",
+    "15:30",
+    "16:00",
+    "16:30",
+    "17:00",
+  ];
+  const staffMembers = mockData.staff.map((staffMember) => {
+    return staffMember.name;
+  });
+  const handleInput = (event) => {
+    switch (event.target.id) {
+      case "time":
+        setTime(event.target.value);
+        break;
+    }
+  };
 
-  const handleName = () => {};
-
-  const handleEmail = () => {};
-
-  const handleNumber = () => {};
-
-  const handleDropdown = () => {};
-
-  const handleSubmit = () => {};
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const firstName = event.target[0].value;
+    const lastName = event.target[1].value;
+    const email = event.target[2].value;
+    const number = event.target[3].value;
+    const checkIfEmailIsValid =
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    const checkIfSpecialCharacters = /^[!@#$%^&*()_+\-=[\]{};':"\\|,.<>/?]*$/;
+    if (firstName == "" || lastName == "" || email == "" || number == "") {
+      alert("Error, please ensure each field isn't empty");
+    } else if (number.length < 10) {
+      alert("Error, phone number must be more than 10 numbers");
+    } else if (/[a-zA-Z]/g.test(number)) {
+      alert("Error, phone number most only contain numbers");
+    } else if (!checkIfEmailIsValid.test(email)) {
+      alert("Error, email is not valid");
+    } else if (/\d/.test(firstName) || /\d/.test(lastName)) {
+      alert("Error, names cannot contain numbers");
+    } else if (
+      checkIfSpecialCharacters.test(firstName) ||
+      checkIfSpecialCharacters.test(lastName)
+    ) {
+      alert("Error, names cannot contain special characters");
+    } else {
+      const objectToSubmit = {
+        firstName: firstName,
+        lastName: lastName,
+        email: email,
+        number: number,
+        staff: event.target[4].value,
+        bookingDate: day,
+        bookingTime: time,
+      };
+      console.log(objectToSubmit);
+      event.target.reset();
+    }
+  };
 
   return (
-    <form className="input-field-container">
+    <form className="input-field-container" onSubmit={handleSubmit}>
       <div className="input-field-container__firstName">
-        <InputField id="first-name" label="First Name" handleInput={handleName} />
+        <InputField id="first-name" label="First Name" />
       </div>
       <div className="input-field-container__lastName">
-        <InputField id="last-name" label="Last Name" handleInput={handleName} />
+        <InputField id="last-name" label="Last Name" />
       </div>
       <div className="input-field-container__email">
-        <InputField id="email" label="Email Address" handleInput={handleEmail} />
+        <InputField id="email" label="Email Address" />
       </div>
       <div className="input-field-container__mobile">
-        <InputField id="number" label="Mobile Number" handleInput={handleNumber} />
+        <InputField id="number" label="Mobile Number" />
       </div>
       <div className="input-field-container__staff-dropdown">
         <DropdownField
           id="staff"
           label="Staff Member"
-          handleInput={handleDropdown}
+          handleInput={handleInput}
+          optionsJSX={staffMembers}
         />
       </div>
       <div id="datePicker" className="input-field-container__date-picker">
@@ -73,22 +132,18 @@ const UserInfoContainer = () => {
         />
       </div>
       <div className="input-field-container__time-dropdown">
-        <DropdownField id="time" label="Time" handleInput={handleDropdown} />
+        <DropdownField
+          id="time"
+          label="Time"
+          handleInput={handleInput}
+          optionsJSX={time_slots}
+        />
       </div>
       <div className="input-field-container__submit">
-        <Button
-          buttonText="Submit"
-          handleSubmit={handleSubmit}
-          buttonStyle="isMobileDefault"
-        />
+        <Button buttonText="Submit" buttonStyle="isMobileDefault" />
       </div>
       <div className="input-field-container__buttons">
-        <Button
-          buttonText="Cancel"
-          handleSubmit={handleSubmit}
-          buttonStyle="isCancel--grey"
-        />
-        <Button buttonText="Save" handleSubmit={handleSubmit} />
+        <Button buttonText="Save" />
       </div>
     </form>
   );
