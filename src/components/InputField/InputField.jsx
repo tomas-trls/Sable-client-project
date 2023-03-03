e1c66a6ff3a41422fdd5fa44827f5b6589023545
DexterFreeman <67049776+DexterFreeman@users.noreply.github.com>
2023-03-03 11:12:16 +0000

    added alerts

--- src/components/InputField/InputField.jsx
+++ src/components/InputField/InputField.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import "./InputField.scss"
 
-const InputField = ({label, placeholderText, id, handleInput, readOnly, editValue}) => {
+const InputField = ({label, placeholderText, id, handleInput, readOnly, editValue, type}) => {
   return (
     <div className='input-field'>
         <label className='input-field__label' htmlFor={id}>{label}</label>
-        <input className="input-field__input" readOnly={readOnly} id={id} type="text" placeholder={placeholderText} onChange={handleInput} defaultValue={editValue}/>
+        <input className="input-field__input" readOnly={readOnly} id={id} type={type} placeholder={placeholderText} onInput={handleInput} defaultValue={editValue}/>
     </div>
   )
 }
