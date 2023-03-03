11c69dfe83bce8a5d5dd49fe5c83ace182810b53
tomas-trls <62387360+tomas-trls@users.noreply.github.com>
2023-03-03 10:42:46 +0000

    Feature 108 resource search filter (#56)
    * end of day commit
    
    * setting up branch
    
    * feature: Resource Page updated to show correct resource requests as well as staff requesting them
    
    * feature: staff resource container updated with styling to show health product requests with relevant staff and their resource requests. With the same for the Admin
    
    * fix: fixed error caused by merge
    
    * fix: removed commetns and white space
    
    ---------
    
    Co-authored-by: TristanP <tristanpere@googlemail.com>

--- src/containers/StaffResourcesContainer/StaffResourcesContainer.jsx
+++ src/containers/StaffResourcesContainer/StaffResourcesContainer.jsx
@@ -1,17 +1,39 @@
 import React from "react";
 import "./StaffResourcesContainer.scss";
 import StaffResources from "../../components/StaffResources/StaffResources";
-import { mockData } from "../../data/mockData"
-const StaffResourcesContainer = () => {
-  return (
-    <div className="staff-resource-container">
+import { mockData } from "../../data/mockData";
+const StaffResourcesContainer = ({ isAdmin }) => {
+  const staffResourceList = (isAdmin) => {
+    let category = "";
+    isAdmin ? (category = "Admin") : (category = "Health");
+    const arrayOfStaff = mockData.staff.map((staff) => {
+      const staffResources = mockData.resourcesRequest.filter(
+        (resourcesRequest) => {
+          return (
+            resourcesRequest.staffMember == staff.name &&
+            resourcesRequest.category == category
+          );
+        }
+      );
+      return [staff.name, staffResources];
+    });
+    const staffRequests = arrayOfStaff.filter((staff) => {
+      return staff[1].length != 0;
+    });
+    return staffRequests;
+  };
+
+  const staffJSX = staffResourceList(isAdmin).map((staff, index) => {
+    return (
       <StaffResources
-        staffName={"Staff 01"}
-        renderLabels={true}
-        cardsArray={mockData.resources}
+        key={index}
+        staffName={staff[0]}
+        cardsArray={staff[1]}
+        id={index}
       />
-      <StaffResources staffName={"Staff 02"} cardsArray={mockData.resources} />
-    </div>
-  );
+    );
+  });
+
+  return <div className="staff-resource-container">{staffJSX}</div>;
 };
 export default StaffResourcesContainer;
